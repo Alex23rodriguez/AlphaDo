3134e342d18cb5e34492b431a8c2c7ef721efc54
Alex23rodriguez <alex.rodriguez.oro@gmail.com>
2019-05-18 20:52:51 -0500

    all should work... but getting fn weird "TypeError: find_st(...) is undefined" even though i can use the function np

--- brute.js
+++ brute.js
@@ -1,4 +1,4 @@
-var used = [[],[],[],[]];
+var used = [[], [], [], []];
 var dran;
 var moves;
 var wins;
@@ -7,333 +7,280 @@ var CHAIN;
 var pr = true;
 
 //recursive
-function play_wrapper(ps, start, chain){
-    //global moves, wins, TIME, CHAIN, PLAYERS
-    moves = make_moves(ps);
-    wins = [0,0,0,0,0];
-    
-    u = [new Set(),new Set(),new Set(),new Set()];
-    let [p, i] = find_st(ps, ...start);
-    CHAIN = [ps[p][i]];
-    u[p].add(int(i));
-    
-    let TIME = new Date();
-    if(chain){
-        play_r_ch(start, (p+1)%4, u, p);
-    }else{
-        play_r(start, (p+1)%4, u, p);
+function play_wrapper(ps, start, chain) {
+  //global moves, wins, TIME, CHAIN, PLAYERS
+  moves = make_moves(ps);
+  wins = [0, 0, 0, 0, 0];
+
+  u = [new Set(), new Set(), new Set(), new Set()];
+  let [p, i] = find_st(ps, ...start);
+  CHAIN = [ps[p][i]];
+  u[p].add(int(i));
+
+  let TIME = new Date();
+  if (chain) {
+    play_r_ch(start, (p + 1) % 4, u, p);
+  } else {
+    play_r(start, (p + 1) % 4, u, p);
+  }
+  TIME = new Date() - TIME;
+  if (pr) {
+    let sum = wins.reduce((a, b) => a + b, 0);
+    console.log('Games played:' + sum);
+    console.log('Time: ' + TIME);
+    console.log('Results:');
+    for (let i of wins) {
+      console.log((i / sum) * 100);
     }
-    TIME = new Date() - TIME;
-    if(pr){
-        let sum = wins.reduce((a, b) => a + b, 0);
-        console.log('Games played:' + sum);
-        console.log('Time: ' + TIME);
-        console.log('Results:');
-        for(let i of wins){
-            console.log(i/sum*100);
-        }
-    }
-    return wins
+  }
+  return wins;
 }
 
-function play_r(b, p, u, last){
-    let st = moves[p][b[0]].slice();
-    for(let s=st.length-1;s>-1;s--){
-        if(u[p].has(st[s][0])){
-            st.splice(s,1);
-        }
+function play_r(b, p, u, last) {
+  let st = moves[p][b[0]].slice();
+  for (let s = st.length - 1; s > -1; s--) {
+    if (u[p].has(st[s][0])) {
+      st.splice(s, 1);
     }
-    let left = st.length;
-    
-    if(b[0]!=b[1]){
-        st = Array.prototype.concat(st,moves[p][b[1]]);
+  }
+  let left = st.length;
+
+  if (b[0] != b[1]) {
+    st = Array.prototype.concat(st, moves[p][b[1]]);
+  }
+  for (let s = st.length - 1; s > left - 1; s--) {
+    if (u[p].has(st[s][0])) {
+      st.splice(s, 1);
     }
-    for(let s=st.length-1;s>left-1;s--){
-        if(u[p].has(st[s][0])){
-            st.splice(s,1);
-        }
+  }
+
+  if (st.length == 0) {
+    if (p == last) {
+      wins[4] += 1;
+      //if(sum(wins)%1000000 == 0){
+      //    console.log(wins)
+      //}
+      return;
     }
-            
-    if(st.length == 0){
-        if(p==last){
-            wins[4] += 1;
-            //if(sum(wins)%1000000 == 0){
-            //    console.log(wins)
-            //}
-            return;
-        }
-        play_r(b, (p+1)%4, u, last);  //did not place
+    play_r(b, (p + 1) % 4, u, last); //did not place
+  }
+  for (let x = 0; x < st.length; x++) {
+    let u2 = [];
+    for (i of u) {
+      u2.push(new Set(i));
     }
-    for(let x=0;x<st.length;x++){
-        let u2 = [];
-        for(i of u){
-            u2.push(new Set(i));
-        }
-        //console.log(st[x])
-        //console.log(st)
-        u2[p].add(int(st[x][0]));
-        if(u2[p].size == moves[p][-1]){ //number of starting pieces
-            wins[p] += 1;
-            //if sum(wins)%1000000 == 0:
-            //    console.log(wins)
-            return;
-        }
-        b2 = b.slice();
-        if(x < left){
-            b2[0] = st[x][1];
-        }else{
-            b2[1] = st[x][1];
-        }
-		// console.log('TCL: functionplay_r -> b2', b2);
-		// console.log('TCL: functionplay_r -> (p+1)%4', (p+1)%4);
-		// console.log('TCL: functionplay_r -> u2', u2);
-		// console.log('TCL: functionplay_r -> p', p);
-        play_r(b2, (p+1)%4, u2, p);
-		// console.log('TCL: functionplay_r -> b2', b2);
-		// console.log('TCL: functionplay_r -> (p+1)%4', (p+1)%4);
-		// console.log('TCL: functionplay_r -> u2', u2);
-		// console.log('TCL: functionplay_r -> p', p);
+    u2[p].add(int(st[x][0]));
+    if (u2[p].size == moves[p][-1]) {
+      //number of starting pieces
+      wins[p] += 1;
+      //if sum(wins)%1000000 == 0:
+      //    console.log(wins)
+      return;
     }
+    b2 = b.slice();
+    if (x < left) {
+      b2[0] = st[x][1];
+    } else {
+      b2[1] = st[x][1];
+    }
+    play_r(b2, (p + 1) % 4, u2, p);
+  }
 }
 
-function find_st(ps, a, b){  // could intergrate moves
-    for(let p=0;p<4;p++){
-        for(i in ps[p]){
-            if(a==ps[p][i][0] && b==ps[p][i][1]){
-                return [p, i];
-            }
-            if(a==ps[p][i][1] && b==ps[p][i][0]){
-                return [p, i];
-            }
-        }
+function find_st(ps, a, b) {
+  // could intergrate moves
+  for (let p = 0; p < 4; p++) {
+    for (i in ps[p]) {
+      if (a == ps[p][i][0] && b == ps[p][i][1]) {
+        return [p, int(i)];
+      }
+      if (a == ps[p][i][1] && b == ps[p][i][0]) {
+        return [p, int(i)];
+      }
     }
-}
-function make_moves(ps){
-    let m = {};
-    for(let e in ps){ //players
-        let pl = ps[e];
-        m[e] = {};
-        for(let i=0;i<7;i++){ //nums
-            m[e][i] = [];
-            m[e][-1] = pl.length; //number of starting pieces
-            for(let j=0;j<pl.length;j++){ //st
-                if(pl[j][0] == i){
-                    m[e][i].push([j, pl[j][1]]);
-                }else if(pl[j][1] == i){
-                    m[e][i].push([j, pl[j][0]]);
-                }
-            }
-        }
-    }
-    return m;
+  }
 }
 
-function pprint(ps){
-    for(p of ps){
-        console.log(p)
+function make_moves(ps) {
+  let m = {};
+  for (let e in ps) {
+    //players
+    let pl = ps[e];
+    m[e] = {};
+    for (let i = 0; i < 7; i++) {
+      //nums
+      m[e][i] = [];
+      m[e][-1] = pl.length; //number of starting pieces
+      for (let j = 0; j < pl.length; j++) {
+        //st
+        if (pl[j][0] == i) {
+          m[e][i].push([j, pl[j][1]]);
+        } else if (pl[j][1] == i) {
+          m[e][i].push([j, pl[j][0]]);
+        }
+      }
     }
+  }
+  return m;
 }
 
+function pprint(ps) {
+  for (p of ps) {
+    console.log(p);
+  }
+}
 
-function play_r_ch(b, p, u, last){
-    //global wins, ps_, CHAIN
-    
-    st = moves[p][b[0]].slice();
+function play_r_ch(b, p, u, last) {
+  //global wins, ps_, CHAIN
 
-    for(let s=st.length-1;s>-1;s--){
-        if(u[p].has(st[s][0])){
-            st.splice(s,1);
-        }
-    }
-            
-    left = st.length;
-    
-    if(b[0]!=b[1]){
-        st = Array.prototype.concat(st,moves[p][b[1]]);
-    }
-    for(let s=st.length-1;s>left-1;s--){
-        if(u[p].has(st[s][0])){
-            st.splice(s,1);
-        }
-    }
+  st = moves[p][b[0]].slice();
 
-    if(st.length == 0){
-        if(p==last){
-            console.log([CHAIN, 'Stuck']);
-            wins[4] += 1;
-            //if(sum(wins)%1000000 == 0){
-            //    console.log(wins)
-            //}
-            return;
-        }
-        CHAIN.push(undefined);
-        play_r_ch(b, (p+1)%4, u, last);  //did not place
-        CHAIN.pop();
+  for (let s = st.length - 1; s > -1; s--) {
+    if (u[p].has(st[s][0])) {
+      st.splice(s, 1);
     }
-            
-    for(let x=0;x<st.length;x++){
-        let u2 = [];
-        for(i of u){
-            u2.push(new Set(i));
-        }
-        u2[p].add(int(st[x][0]));
-        if(u2[p].size == moves[p][-1]){ //number of starting pieces
-            if(x<left){
-                CHAIN.push([ps_[p][st[x][0]], 'l']);
-            }else{
-                CHAIN.push([ps_[p][st[x][0]], 'r']);
-            }
-            wins[p] += 1;
-            console.log(CHAIN, p);
-            CHAIN.pop();
-            return;
-        }
-        b2 = b.slice();
-        if(x < left){
-            b2[0] = st[x][1];
-            CHAIN.push((ps_[p][st[x][0]], 'l'));
-        }else{
-            b2[1] = st[x][1];
-            CHAIN.push((ps_[p][st[x][0]], 'r'))
-        }
-        play_r_ch(b2, (p+1)%4, u2, p);
-        CHAIN.pop();
+  }
+
+  left = st.length;
+
+  if (b[0] != b[1]) {
+    st = Array.prototype.concat(st, moves[p][b[1]]);
+  }
+  for (let s = st.length - 1; s > left - 1; s--) {
+    if (u[p].has(st[s][0])) {
+      st.splice(s, 1);
     }
+  }
+
+  if (st.length == 0) {
+    if (p == last) {
+      console.log([CHAIN, 'Stuck']);
+      wins[4] += 1;
+      //if(sum(wins)%1000000 == 0){
+      //    console.log(wins)
+      //}
+      return;
+    }
+    CHAIN.push(undefined);
+    play_r_ch(b, (p + 1) % 4, u, last); //did not place
+    CHAIN.pop();
+  }
+
+  for (let x = 0; x < st.length; x++) {
+    let u2 = [];
+    for (i of u) {
+      u2.push(new Set(i));
+    }
+    u2[p].add(int(st[x][0]));
+    if (u2[p].size == moves[p][-1]) {
+      //number of starting pieces
+      if (x < left) {
+        CHAIN.push([ps_[p][st[x][0]], 'l']);
+      } else {
+        CHAIN.push([ps_[p][st[x][0]], 'r']);
+      }
+      wins[p] += 1;
+      console.log(CHAIN, p);
+      CHAIN.pop();
+      return;
+    }
+    b2 = b.slice();
+    if (x < left) {
+      b2[0] = st[x][1];
+      CHAIN.push((ps_[p][st[x][0]], 'l'));
+    } else {
+      b2[1] = st[x][1];
+      CHAIN.push((ps_[p][st[x][0]], 'r'));
+    }
+    play_r_ch(b2, (p + 1) % 4, u2, p);
+    CHAIN.pop();
+  }
+}
+
+const setDifference = (a, b) => new Set([...a].filter(x => !b.has(x)));
+
+function validHand(hand, empty) {
+  for (i in hand) {
+    let nums = new Set();
+    for (s of hand[i]) {
+      nums.add(s[0]);
+      nums.add(s[1]);
+    }
+    for (n of empty[i]) {
+      if (nums.has(n)) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 function hands(stones, count, empty) {
-    var stoneset = new Set(stones);
-    var choose = [];
-    var conf = [];
-    for (let i = 0; i < count[0]; i++) {
-        conf.push(i);
+  var stoneset = new Set(stones);
+  var choose = [];
+  var conf = [];
+  for (let i = 0; i < count[0]; i++) {
+    conf.push(i);
+  }
+  var i = count[0] - 1;
+  while (true) {
+    while (conf[i] < stones.length) {
+      var n = [];
+      for (let j of conf) {
+        n.push(stones[j]);
+      }
+
+      if (!validHand([n], [empty[0]])) {
+        conf[i] += 1;
+        continue;
+      }
+
+      if (count.length > 1) {
+        var n2 = new Set(n);
+        var t = [...setDifference(stoneset, n2)];
+
+        for (j of hands(t, count.slice(1), empty.slice(1))) {
+          choose.push([n, ...j]);
+        }
+      } else {
+        choose.push([n]);
+      }
+      conf[i] += 1;
     }
-    var i = count[0] - 1;
-    while (true) {
-        while (conf[i] < stones.length) {
-            var n = [];
-            for (let j of conf) {
-                n.push(stones[j]);
-            }
-
-            if(!validHand([n], [empty[0]])){
-                conf[i]+=1;
-                continue;
-            }
-
-            if (count.length > 1) {
-                var n2 = new Set(n);
-                var t = [...setDifference(stoneset, n2)];
-
-                for (j of hands(t, count.slice(1), empty.slice(1))) {
-                    choose.push([n, ...j]);
-                }
-            } else {
-                choose.push([n]);
-            }
-            conf[i] += 1;
-        }
-        i -= 1
-        while (i >= 0 && conf[i] + 1 == conf[i + 1]) {
-            i -= 1;
-        }
-        if (i == -1) {
-            break;
-        }
-        conf[i] = conf[i] + 1;
-        while (i < count[0] - 1) {
-            conf[i + 1] = conf[i] + 1;
-            i += 1;
-        }
+    i -= 1;
+    while (i >= 0 && conf[i] + 1 == conf[i + 1]) {
+      i -= 1;
     }
-    return choose;
+    if (i == -1) {
+      break;
+    }
+    conf[i] = conf[i] + 1;
+    while (i < count[0] - 1) {
+      conf[i + 1] = conf[i] + 1;
+      i += 1;
+    }
+  }
+  return choose;
 }
 
-//brute
-/*
+function generateHands() {
+  // sorts empty desc to optimize choose function, then applies inverse sort!
+  let otras = fichas.filter(x => !misFichas.includes(x));
 
-function turn2(p, b){
-    let st = moves[p][b[0]].slice();
-    for(let s=st.length-1;s>-1;s--){
-        if(used[p].includes(st[s][0])){
-            st.splice(s, 1);
-        }
-    }
-    let left = st.length;
-    
-    if(board[0]!=board[1]){
-        st = st.concat(moves[p][b[1]]);
-    }
-    for(let s=st.length-1;s>left-1;s--){
-        if(used[p].includes(st[s][0])){
-            st.splice(s,1);
-        }
-    }
+  let em = empty.slice(1); // dont consider player
+  let e = em.map(x => x.length);
+  //sort both elemets desc, according to the ammount of empty hands
+  let em2 = em.slice().sort((i, j) => e[em.indexOf(i)] < e[em.indexOf(j)]);
+  let count = fichaCount.slice(1);
+  count.sort((i, j) => e[count.indexOf(i)] < e[count.indexOf(j)]);
+  let hs = hands(otras, count, em2);
+  let sort_inverse = (i, j) => e[h.indexOf(i)] < e[h.indexOf(j)];
 
-    if(st.length == 0){ //did not place
-        return [b, false];
-    }
-    
-    let x = int(random(st.length));
-    
-    used[p].push(st[x][0]) //use up index
-    if(x < left){
-        b[0] = st[x][1];
-    }else{
-        b[1] = st[x][1];
-    }
-    return [b, true];        //did place
+  let ans = [];
+  for (h of hs) {
+    let h2 = h.sort(sort_inverse);
+    h2.unshift(misRest);
+    ans.push(h2);
+  }
+  return ans;
 }
-
-function choose(st){
-    //## random
-    x = st[int(random(st.length))];
-    //## order
-    // x = st[0]
-    return x
-}
-
-function play(ps, b, start, pr=false){
-    //global moves
-    //global dran, used
-    used = [[],[],[],[]];
-    let [dran, i] = find(ps, ...start);
-    used[dran].push(i);
-
-    last = dran
-    while(true){
-        dran = (dran+1)%4;
-        [b, placed] = turn2(dran, b, pr);
-        if(placed && used[dran].length==moves[dran][-1]){ // finished
-            if(pr){
-                console.log(used);
-            }
-            return dran;
-        }
-        if(placed){
-            last = dran;
-        }else if(last==dran){ // stuck
-            if(pr){
-                console.log(used);
-            }
-            return -1;
-        }
-    }
-}
-
-function play_n(ps_, games=10000, pr=false){
-    //global moves
-    moves = make_moves(ps_);
-    wins = [0,0,0,0,0];
-    for(i=0;i<games;i++){
-        let ps = [];
-        for(p of ps_){
-            ps.push(p.slice());
-        }
-        wins[play(ps, pr)] += 1  // play the game
-    }
-    for(i of wins){
-        console.log(round(i/games*100,2));
-    }
-}
-*/
--- sketch.js
+++ sketch.js
@@ -4,303 +4,231 @@ let startB = [];
 
 let fichas = [];
 let misFichas = [];
+let misRest = [];
 let buttons = [];
-let fichaCount = [7,7,7,7]
+let fichaCount = [7, 7, 7, 7];
 
-let empty = [[],[],[],[]];
+let empty = [[], [], [], []];
 
 let ambiButton = [];
 
 let board = undefined;
 
-names = {0:"you", 1:"right", 2:"your teammate", 3:"left"}
+names = { 0: 'you', 1: 'right', 2: 'your teammate', 3: 'left' };
 
 function setup() {
-    createCanvas(window.windowWidth, window.windowHeight);
-    let x = width/7;
-    let y = height/4*0.8;
-    let z = 0;
-    for(let i=0;i<7;i++){
-        buttons.push([]);
-        for(let j=i;j<7;j++){
-            let l = [i,j];
-            fichas.push(l);
-            buttons[l] = createButton(i+"  "+j).position(x*(z%7), y*int(z/7)).size(x,y);
-            buttons[l].mousePressed(function(){
-                if(canClick(l)){
-                    this.hide();
-                    if(phase==2){
-                        fichaCount[turn]-=1;
-                    }
-                    decide(l);
-                }
-            });
-            z++;
+  createCanvas(window.windowWidth, window.windowHeight);
+  let x = width / 7;
+  let y = (height / 4) * 0.8;
+  let z = 0;
+  for (let i = 0; i < 7; i++) {
+    buttons.push([]);
+    for (let j = i; j < 7; j++) {
+      let l = [i, j];
+      fichas.push(l);
+      buttons[l] = createButton(i + '  ' + j)
+        .position(x * (z % 7), y * int(z / 7))
+        .size(x, y);
+      buttons[l].mousePressed(function() {
+        if (canClick(l)) {
+          this.hide();
+          if (phase == 2) {
+            fichaCount[turn] -= 1;
+          }
+          decide(l);
         }
+      });
+      z++;
     }
+  }
 
-    ambiButton.push(createButton("Left value").position(width*.4, height*.9).size(60,20));
-    ambiButton.push(createButton("Right value").position(width*.6, height*.9).size(60,20));
-    for(b of ambiButton){
-        b.hide();
-    }
+  ambiButton.push(
+    createButton('Left value')
+      .position(width * 0.4, height * 0.9)
+      .size(60, 20)
+  );
+  ambiButton.push(
+    createButton('Right value')
+      .position(width * 0.6, height * 0.9)
+      .size(60, 20)
+  );
+  for (b of ambiButton) {
+    b.hide();
+  }
 
-    info();
-    //drawBoard();
+  info();
+  //drawBoard();
 }
 
-function startingP(t){
-    return function(){
-        for(b of startB){
-            b.hide();
-        }
-        turn = t-1;
-        phase=2;
-        createButton("PASS").position(width*0.8, height*0.9).size(80,20).mousePressed(pass);
-        decide();
+function startingP(t) {
+  return function() {
+    for (b of startB) {
+      b.hide();
     }
+    turn = t - 1;
+    phase = 2;
+    createButton('PASS')
+      .position(width * 0.8, height * 0.9)
+      .size(80, 20)
+      .mousePressed(pass);
+    decide();
+  };
 }
 
-function pass(){
-    let emp = new Set(empty[turn]);
-    emp.add(board[0]);
-    emp.add(board[1]);
-    empty[turn] = [...emp];
-    decide()
+function pass() {
+  let emp = new Set(empty[turn]);
+  emp.add(board[0]);
+  emp.add(board[1]);
+  empty[turn] = [...emp];
+  decide();
 }
 
-function canClick(l){
-    if(phase<2 || board==undefined){
-        return abs(phase)==1 ? false : true;
-    }
-    return board.includes(l[0]) || board.includes(l[1]);
+function canClick(l) {
+  if (phase < 2 || board == undefined) {
+    return abs(phase) == 1 ? false : true;
+  }
+  return board.includes(l[0]) || board.includes(l[1]);
 }
 
-function info(){
-    background(0);
-    fill(255);
-    let s;
-    if(phase==0){
-        s = "Pick "+(7-turn)+" stones";
-    }else if(phase==1){
-        s = "Who started?"
-    }else if(phase==2){
-        s = "What did "+names[turn]+" place?" 
-    }else if(phase==-1){
-        s = "Which number was connected?"
-    }
-    text(s,30, height*0.9)
-    if(board){
-        text(board, 30, height*0.95);
-        text(fichaCount, 30, height*0.85);
-    }
+function info() {
+  background(0);
+  fill(255);
+  let s;
+  if (phase == 0) {
+    s = 'Pick ' + (7 - turn) + ' stones';
+  } else if (phase == 1) {
+    s = 'Who started?';
+  } else if (phase == 2) {
+    s = 'What did ' + names[turn] + ' place?';
+  } else if (phase == -1) {
+    s = 'Which number was connected?';
+  }
+  text(s, 30, height * 0.9);
+  if (board) {
+    text(board, 30, height * 0.95);
+    text(fichaCount, 30, height * 0.85);
+  }
 }
 
-function decide(l){
-    if(phase==0){
-        turn++;
-        misFichas.push(l);
-        if(turn==7){
-            phase = 1;
-            turn = 0;
-            startB.push(createButton("Me").position(width/2, height*0.95).size(60,20).mousePressed(startingP(0)));
-            startB.push(createButton("Right").position(width*0.6, height*0.9).size(60,20).mousePressed(startingP(1)));
-            startB.push(createButton("Team").position(width/2, height*0.85).size(60,20).mousePressed(startingP(2)));
-            startB.push(createButton("Left").position(width*0.4, height*0.9).size(60,20).mousePressed(startingP(3)));
-        }
-    }else if(phase==2){
-        if(l){
-            fichas.splice(fichas.indexOf(l),1);
-            if(!board){
-                board = l;
-            }else{
-                updateBoard(l);
-            }
-        }
-        turn++;
-        turn%=4;
-        if(turn==0){
-            for(let l_ of fichas){
-                if(misFichas.includes(l_)){
-                    buttons[l_].show();
-                }
-                else{
-                    buttons[l_].hide();
-                }
-            }
-        }else if(turn==1){
-            for(l_ of fichas){
-                if(misFichas.includes(l_)){
-                    buttons[l_].hide()
-                }
-                else{
-                    buttons[l_].show();
-                }
-            }
-        }
-    }else if(phase==-1){
-        
-        ambiButton[0].show();
-        ambiButton[0].elt.innerHTML = l[0];
-        ambiButton[0].mousePressed(function(){
-            board[board.indexOf(l[0])] = l[1];
-            ambiButton[0].hide();
-            ambiButton[1].hide();
-            phase=2;
-        });
-        ambiButton[1].show();
-        ambiButton[1].elt.innerHTML = l[1];
-        ambiButton[1].mousePressed(function(){
-            board[board.indexOf(l[1])] = l[0];
-            ambiButton[0].hide();
-            ambiButton[1].hide();
-            phase=2;
-        }); 
-
+function decide(l) {
+  if (phase == 0) {
+    turn++;
+    misFichas.push(l);
+    if (turn == 7) {
+      misRest = misFichas.slice();
+      phase = 1;
+      turn = 0;
+      startB.push(
+        createButton('Me')
+          .position(width / 2, height * 0.95)
+          .size(60, 20)
+          .mousePressed(startingP(0))
+      );
+      startB.push(
+        createButton('Right')
+          .position(width * 0.6, height * 0.9)
+          .size(60, 20)
+          .mousePressed(startingP(1))
+      );
+      startB.push(
+        createButton('Team')
+          .position(width / 2, height * 0.85)
+          .size(60, 20)
+          .mousePressed(startingP(2))
+      );
+      startB.push(
+        createButton('Left')
+          .position(width * 0.4, height * 0.9)
+          .size(60, 20)
+          .mousePressed(startingP(3))
+      );
     }
+  } else if (phase == 2) {
+    if (l) {
+      fichas.splice(fichas.indexOf(l), 1);
+      if (turn == 0) {
+        misRest.splice(misRest.indexOf(l), 1);
+      }
+      if (!board) {
+        board = l;
+      } else {
+        updateBoard(l);
+      }
+    }
+    turn++;
+    turn %= 4;
+    if (turn == 0) {
+      for (let l_ of fichas) {
+        if (misFichas.includes(l_)) {
+          buttons[l_].show();
+        } else {
+          buttons[l_].hide();
+        }
+      }
+    } else if (turn == 1) {
+      for (l_ of fichas) {
+        if (misFichas.includes(l_)) {
+          buttons[l_].hide();
+        } else {
+          buttons[l_].show();
+        }
+      }
+    }
+  } else if (phase == -1) {
+    ambiButton[0].show();
+    ambiButton[0].elt.innerHTML = l[0];
+    ambiButton[0].mousePressed(function() {
+      board[board.indexOf(l[0])] = l[1];
+      ambiButton[0].hide();
+      ambiButton[1].hide();
+      phase = 2;
+    });
+    ambiButton[1].show();
+    ambiButton[1].elt.innerHTML = l[1];
+    ambiButton[1].mousePressed(function() {
+      board[board.indexOf(l[1])] = l[0];
+      ambiButton[0].hide();
+      ambiButton[1].hide();
+      phase = 2;
+    });
+  }
 }
 
-function updateBoard(l){
-    if(board[0]==board[1]){
-        if(board[0]==l[0]){
-            board[0] = l[1];
-        }else{
-            board[0] = l[0];
-        }
-        return;
+function updateBoard(l) {
+  if (board[0] == board[1]) {
+    if (board[0] == l[0]) {
+      board[0] = l[1];
+    } else {
+      board[0] = l[0];
     }
-    if(board[0]==l[0]){
-        if(board[1]==l[1]){
-            phase = -1;
-            console.log("oh no!");
-            decide(l);
-        }else{
-            board[0] = l[1];
-        }
-    }else if(board[0]==l[1]){
-        if(board[1]==l[0]){
-            phase = -1;
-            console.log("oh no!");
-            decide(l);
-        }else{
-            board[0] = l[0];
-        }
-    }else if(board[1]==l[0]){
-        board[1] = l[1];
-    }else{
-        board[1] = l[0];
+    return;
+  }
+  if (board[0] == l[0]) {
+    if (board[1] == l[1]) {
+      phase = -1;
+      console.log('oh no!');
+      decide(l);
+    } else {
+      board[0] = l[1];
     }
+  } else if (board[0] == l[1]) {
+    if (board[1] == l[0]) {
+      phase = -1;
+      console.log('oh no!');
+      decide(l);
+    } else {
+      board[0] = l[0];
+    }
+  } else if (board[1] == l[0]) {
+    board[1] = l[1];
+  } else {
+    board[1] = l[0];
+  }
 }
 
-function mousePressed(){
-    background(0);
-    info();
+function mousePressed() {
+  background(0);
+  info();
 }
 
-const setDifference = (a, b) => new Set([...a].filter(x => !b.has(x)));
-
-function validHand(hand, empty){
-    for(i in hand){
-        let nums = new Set();
-        for(s of hand[i]){
-            nums.add(s[0]);
-            nums.add(s[1]);
-        }
-        for(n of empty[i]){
-            if(nums.has(n)){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-function draw() {
-
-}
-
-
-/*
-function hands_old(nums, count) {
-    var numset = new Set(nums);
-    var choose = [];
-    var conf = [];
-    for (let i = 0; i < count[0]; i++) {
-        conf.push(i);
-    }
-    var i = count[0] - 1;
-    while (true) {
-        while (conf[i] < nums.length) {
-            var n = [];
-            for (let j of conf) {
-                n.push(nums[j]);
-            }
-            if (count.length > 1) {
-                var n2 = new Set(n);
-                var t = [...setDifference(numset, n2)];
-
-                for (j of hands_old(t, count.slice(1))) {
-                    choose.push([n, ...j]);
-                }
-            } else {
-                choose.push([n]);
-            }
-            conf[i] += 1;
-        }
-        i -= 1
-        while (i >= 0 && conf[i] + 1 == conf[i + 1]) {
-            i -= 1;
-        }
-        if (i == -1) {
-            break;
-        }
-        conf[i] = conf[i] + 1;
-        while (i < count[0] - 1) {
-            conf[i + 1] = conf[i] + 1;
-            i += 1;
-        }
-    }
-    return choose;
-}
-
-function numInHand(hand, num){
-    for(s of hand){
-        if(num==s[0] || num==s[1]){
-            return true;
-        }
-    }
-    return false;
-}
-
-function reduceHands(hands, empty){
-    let trimmed = [];
-    for(hand of hands){
-        if(validHand(hand, empty)){
-            trimmed.push(hand);
-        }
-    }
-    return(trimmed);
-}
-*/
-
-/*
-function drawBoard(){
-    background(0)
-    let x = width/7*0.9;
-    let y = height/4*0.8;
-    let z = 0;
-    fill(0);
-    for(let i=0;i<7;i++){
-        for(let j=i;j<7;j++){
-
-            if(fichas[""+i+j]){
-                rect(x*(z%7), y*int(z/COUNT), x, y);
-            }
-            text(i,x*(z%7+.25),y*(int(z/COUNT)+.5));
-            text(j,x*(z%7+.75),y*(int(z/COUNT)+.5));
-            z++;
-        }
-    }
-    text(info(), 10, height-40)
-}
-*/
+function draw() {}
